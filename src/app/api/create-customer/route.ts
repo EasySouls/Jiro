5c7d5add446ade2818c971c4891bcd9d6278b3da
EasySouls <tarjanyicsanad@gmail.com>
2024-02-03 13:14:00 +0100

    Create customer Route Handler

---
+++ src/app/api/create-customer/route.ts
@@ -0,0 +1,45 @@
+import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+import { stripe } from '@/lib/stripe';
+
+export async function POST(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const query = searchParams.get('API_ROUTE_SECRET');
+  if (query !== process.env.API_ROUTE_SECRET) {
+    return new NextResponse('Unauthorized to call this API', { status: 401 });
+  }
+
+  try {
+    const data = await req.json();
+
+    if (!data.record.email) {
+      return new NextResponse('Email is required', { status: 400 });
+    } else if (!data.record.id) {
+      return new NextResponse('User ID is required', { status: 400 });
+    }
+
+    const customer = await stripe.customers.create({
+      email: data.record.email,
+    });
+
+    const supabase = createClient(cookies());
+
+    const { error } = await supabase
+      .from('profiles')
+      .update({
+        stripe_customer: customer.id,
+      })
+      .eq('id', data.record.id);
+
+    if (error) {
+      return new NextResponse(error.message, { status: 500 });
+    }
+
+    return new NextResponse(JSON.stringify(customer));
+  } catch (error) {
+    return new NextResponse("Couldn't create a new Stripe customer", {
+      status: 500,
+    });
+  }
+}
--- src/app/api/stripe/route.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from 'stripe';
-import { createClient } from '@/lib/supabase/server';
-import { cookies } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function POST(req: NextRequest, res: NextResponse) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-}
